616eb7f9a8fc38dcc0b21192ec09d70bcc65d484
Seraf00 <53179542+Seraf00@users.noreply.github.com>
2021-01-05 14:43:44 +0100

    Loop To more Dice & sum

--- dice_roller.py
+++ dice_roller.py
@@ -1,5 +1,11 @@
 def main():
-  print('You rolled a die')
-
+  import random
+  dice_roll = 2
+  dice_sum = 0
+  for i in range(0,dice_roll):
+     roll =random.randint(1,6)
+     print(f'You rolled a {roll}')
+     dice_sum = dice_sum + roll # we can use also dice_sum += roll
+  print(f'You rolled {dice_sum}')
 if __name__== "__main__":
   main()
